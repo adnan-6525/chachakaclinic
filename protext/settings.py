22eff0e158ad18fa0b784dff32eddf5c1fb17be3
adnan-6525 <136156495+adnan-6525@users.noreply.github.com>
2024-01-13 19:39:27 +0530

    Update settings.py

--- protext/settings.py
+++ protext/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8@g)*z*28it5@fvlnlf5hi427aa!binz_p-z68@!%zy2jqdvt5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
