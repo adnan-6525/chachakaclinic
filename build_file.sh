144c50aa8b93bc39276ca475c228bd441e88edfe
adnan-6525 <136156495+adnan-6525@users.noreply.github.com>
2024-01-13 17:47:28 +0530

    Update build_file.sh

--- build_file.sh
+++ build_file.sh
@@ -1,17 +1,5 @@
-#!/bin/bash
-echo "Build started!"
 
-# Install Python dependencies
-pip install -r requirements.txt
-
-# Apply database migrations
-python manage.py migrate
-
-# Collect static files
-python manage.py collectstatic --noinput
-
-# Create a temporary file to indicate successful build
-touch .vercel-build-success
-
-# Print a message to indicate the build is complete
-echo "Build completed successfully!"
+echo "BUILD START"
+pyhton3.10 -m pip install -r requirement.txt
+python3.10 manage.py collectstatic --noinput --clear
+echo "BUILD END"
